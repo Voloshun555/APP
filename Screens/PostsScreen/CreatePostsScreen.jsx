b468b06e762768544dade72ae5751fe5d7f64d6d
Voloshun555 <voloshunihor@gmail.com>
2023-07-11 17:29:19 +0300

    789

--- Screens/PostsScreen/CreatePostsScreen.jsx
+++ Screens/PostsScreen/CreatePostsScreen.jsx
@@ -1,5 +1,5 @@
-import { Text, View, TouchableOpacity, ImageBackground } from "react-native";
-import { AntDesign, Feather } from "@expo/vector-icons";
+import { Text, View, TouchableOpacity, ImageBackground,ScrollView ,TextInput} from "react-native";
+import { AntDesign, Feather,Entypo } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 import { FormCamera } from "./formCamera";
 import { CreatCamera } from "../creatCamera/CreatCamera";
@@ -72,7 +72,24 @@ export function CreatePostsScreen({ navigation }) {
             <Text>Завантажте фото</Text>
           </TouchableOpacity>
 
-          <FormCamera />
+          <ScrollView>
+      <View
+        style={{
+          ...styles.inputContanier,
+          
+        }}>
+        <TextInput placeholder="Name..." style={styles.input} />
+      </View>
+      <View
+        style={{
+          ...styles.inputContanier,
+        }}>
+        <TextInput  style={styles.input}><Entypo name="location-pin" size={24} color="grey" /> Місцевість...</TextInput>
+      </View>
+<TouchableOpacity style={styles.addPhotoContainer}>
+<Text style={styles.addTextPhoto}>Опублікувати</Text>
+</TouchableOpacity>
+    </ScrollView>
           <View style={styles.deleteContainer}>
             <TouchableOpacity
               style={styles.deleteIcon}
@@ -90,6 +107,34 @@ export function CreatePostsScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  inputContanier: {
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "transparent",
+    borderBottomColor: "#E8E8E8",
+    height: 50,
+    backgroundColor: "transparent",
+    width: "100%",
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+    width: "100%",
+    height: 50,
+  },
+  addPhotoContainer:{
+    backgroundColor: '#F6F6F6',
+    height: 40,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addTextPhoto:{
+    color: '#BDBDBD'
+  },
   iconCamera: {
     transform: [{ translateX: 18 }, { translateY: 18 }],
   },
